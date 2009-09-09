8130104d18a7ccd4e3c6778b3c19a265e2f2df32
ptrthomas <ptrthomas@1fa7ec76-dee5-11dd-975f-53f86f19d922>
2009-09-09 18:07:58 +0000

    tapestry version initial import

---
+++ trunk/perfbench/tapestry-jpa/src/main/java/tapestryjpa/tapestry/services/AuthDispatcher.java
@@ -0,0 +1,35 @@
+package tapestryjpa.tapestry.services;
+
+import java.io.IOException;
+import org.apache.tapestry5.services.ApplicationStateManager;
+import org.apache.tapestry5.services.Dispatcher;
+import org.apache.tapestry5.services.Request;
+import org.apache.tapestry5.services.Response;
+import tapestryjpa.web.BookingSession;
+
+public class AuthDispatcher implements Dispatcher {
+
+    private ApplicationStateManager asm;
+
+    public AuthDispatcher(ApplicationStateManager asm) {
+        this.asm = asm;
+    }
+
+    public boolean dispatch(Request request, Response response) throws IOException {
+        String path = request.getPath();        
+        if (path.equals("/homepage") || path.equals("/homepage.form")
+                || path.startsWith("/assets/")
+                || path.equals("/registerpage")) {
+            return false;
+        }
+        if (asm.exists(BookingSession.class)) {
+            BookingSession session = asm.get(BookingSession.class);            
+            if(session.getUser() != null) {
+                return false;
+            }            
+        }
+        response.sendRedirect(request.getContextPath() + "/homepage");
+        return true;
+    }
+    
+}
